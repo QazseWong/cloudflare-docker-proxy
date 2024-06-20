891baefa40162e9ddd3034ccf634854d0ee3883e
QazseWong <46583229+QazseWong@users.noreply.github.com>
2024-06-20 11:30:32 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.220306.xyz": dockerHub,
+  "quay.220306.xyz": "https://quay.io",
+  "gcr.220306.xyz": "https://gcr.io",
+  "k8s-gcr.220306.xyz": "https://k8s.gcr.io",
+  "k8s.220306.xyz": "https://registry.k8s.io",
+  "ghcr.220306.xyz": "https://ghcr.io",
+  "cloudsmith.220306.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.220306.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
